c32612bc1e0f6aa47d8d61c1b4e7d37ac919e900
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2020-06-04 21:14:04 +0200

    Fix main directory creation on hdfs

--- hadoop-examples/scripts/setup_hdfs.sh
+++ hadoop-examples/scripts/setup_hdfs.sh
@@ -1,8 +1,8 @@
-hdfs dfs -mkdir /user/root
-hdfs dfs -mkdir /user/root/ex1/input
-hdfs dfs -mkdir /user/root/ex2/input
-hdfs dfs -mkdir /user/root/ex3/input
-hdfs dfs -mkdir /user/root/ex4/input
+hdfs dfs -mkdir -p /user/root
+hdfs dfs -mkdir -p /user/root/ex1/input
+hdfs dfs -mkdir -p /user/root/ex2/input
+hdfs dfs -mkdir -p /user/root/ex3/input
+hdfs dfs -mkdir -p /user/root/ex4/input
 hdfs dfs -copyFromLocal /files/ex1/ /user/root/ex1/input/
 hdfs dfs -copyFromLocal /files/ex2/ /user/root/ex2/input/
 hdfs dfs -copyFromLocal /files/ex3/ /user/root/ex3/input/
